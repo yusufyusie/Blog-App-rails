24ec5b1954b26408a9341491bb149d450bb4419a
yusufyusie <yesuf023@gmail.com>
2023-12-09 10:31:41 +0300

    Add validation to user.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comment_counter, numericality: { greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { greater_than_or_equal_to: 0 }
+
   # Updates the posts counter for a user
   private
 
