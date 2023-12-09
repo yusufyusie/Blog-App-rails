34274e4e81b03a4894c2f9c5b04da7ede5120884
yusufyusie <yesuf023@gmail.com>
2023-12-09 11:53:59 +0300

    Add test for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,65 +1,70 @@
 require_relative '../rails_helper'
 
-RSpec.describe User, type: :model do
+RSpec.describe Post, type: :model do
+  let(:user) { User.create(name: 'Fuad', posts_counter: 0) }
   subject do
-    User.new(
-      name: 'John Doe',
-      photo: 'https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg',
-      bio: 'Hello I am a test user',
-      posts_counter: 0
-    )
+    Post.new(user_id: user.id, title: 'Hello', text: 'This is my post', comment_counter: 1, likes_counter: 1)
   end
 
-  describe 'Validations' do
-    it 'is valid with valid attributes' do
-      expect(subject).to be_valid
-    end
-
-    it 'is not valid without a name' do
-      subject.name = ''
+  context 'Testing validation' do
+    it 'Title should be invalid with nil value' do
+      subject.title = nil
       expect(subject).to_not be_valid
     end
 
-    it 'is valid without a photo' do
-      subject.photo = ''
-      expect(subject).to be_valid
+    it 'Title must not exceed 250 characters' do
+      expect(subject.title.length).to be <= 250
     end
 
-    it 'is not valid when posts_counter is not an integer' do
-      subject.posts_counter = 'a'
+    it 'CommentsCounter must be an integer greater than or equal to zero.' do
+      subject.comment_counter = -1
       expect(subject).to_not be_valid
     end
 
-    it 'is not valid when posts_counter is less than 0' do
-      subject.posts_counter = -1
+    it 'LikesCounter must be an integer greater than or equal to zero.' do
+      subject.likes_counter = -1
       expect(subject).to_not be_valid
     end
-  end
-end
 
-context 'Testing behavior' do
-  subject do
-    User.new(
-      name: 'John Doe',
-      photo: 'https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg',
-      bio: 'Hello I am a test user',
-      posts_counter: 0
-    )
+    it 'Should update count of posts' do
+      expect(subject.post_count_updater).to be true
+    end
   end
 
-  before do
-    10.times do |i|
-      Post.create(
-        title: "Post #{i}",
-        text: "This is the text for post #{i}",
-        user: subject, # Assign the user instance to the user attribute
+  describe 'Functionality' do
+    let(:user) do
+      User.create(
+        name: 'John Doe',
+        photo: 'https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg',
+        bio: 'Hello I am a test user',
+        posts_counter: 0
+      )
+    end
+
+    subject do
+      Post.new(
+        title: 'Test Post',
+        text: 'This is a test post',
+        user:,
         comment_counter: 0,
         likes_counter: 0
       )
     end
-  end
 
-  it 'lists the most recent 3 posts' do
-    expect(subject.recent_post.length).to eq(3)
+    it 'returns the five most recent comments' do
+      10.times do |i|
+        Comment.create(
+          text: "This is the text for comment #{i}",
+          post: subject,
+          user:
+        )
+      end
+
+      expect(subject.recent_comments.length).to eq(5)
+    end
+
+    it 'updates the posts_counter of the author after saving' do
+      expect { subject.save }.to change { user.posts_counter }.by(1)
+    end
   end
 end
